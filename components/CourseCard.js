935fd83304e749f29da0d6b0f2811ac8ed33b4c7
sankalp nihal pandey <sankalp@geekyants.com>
2023-02-01 13:28:57 +0530

    added card animation

--- components/CourseCard.js
+++ components/CourseCard.js
@@ -9,64 +9,82 @@ import {
   CardFooter,
   CardBody,
   Button,
-  HStack,
+  chakra,
+  Container,
+  shouldForwardProp,
 } from "@chakra-ui/react";
+import {
+  motion,
+  useTime,
+  useTransform,
+  isValidMotionProp,
+} from "framer-motion";
 import { useRouter } from "next/router";
+
+const ChakraBox = chakra(motion.div, {
+  shouldForwardProp: (prop) =>
+    isValidMotionProp(prop) || shouldForwardProp(prop),
+});
+
 export default function CardComponent(item) {
   const router = useRouter();
   return (
-    <Card
-      minW={280}
-      maxW={280}
-      h="100%"
-      border="2px"
-      borderColor="whiteAlpha.300"
-      alignItems="stretch"
-    >
-      <CardBody h="100%" w="full" justifyContent="space-around">
-        <Image
-          width={"full"}
-          h={200}
-          src={item.imageUri}
-          alt="Green double couch with wooden legs"
-          borderRadius="lg"
-        />
-        <Stack mt="3" spacing="2">
-          <Heading size="md" color="text.900" fontWeight="semibold">
-            {item.courseName}
-          </Heading>
-          <Text textColor="text.900" fontSize="sm" fontWeight="normal">
-            {item.authorName}
-          </Text>
-          <Text color="text.900" fontSize="sm" fontWeight="thin">
-            {item.rating}
-          </Text>
-          <Text color="text.900" fontSize="sm" fontWeight="semibold">
-            ${item.price}
-          </Text>
-        </Stack>
-      </CardBody>
-      <Divider />
-      {!item.isBought ? (
-        <CardFooter>
-          <Button
-            // variant='ghost'
-            color="white"
-            backgroundColor="primary.900"
-            _hover={{ backgroundColor: "hover.900" }}
-            onClick={() => {
-              router.push({
-                pathname: item.isInstructor
-                  ? "/instructor/course-edit"
-                  : "/student/course-info",
-                query: { _id: item._id },
-              });
-            }}
-          >
-            See Details
-          </Button>
-        </CardFooter>
-      ) : null}
-    </Card>
+    <Container display="flex" alignItems="center" justifyContent="center">
+      <Card minW={280} maxW={280}>
+        <ChakraBox
+          // @ts-ignore no problem in operation, although type error appears.
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: "spring", stiffness: 500, damping: 120 }}
+          width="280px"
+          height="100%"
+          border-radius="20px"
+          cursor="pointer"
+        >
+          <CardBody h="100%" w="full" justifyContent="space-around">
+            <Image
+              width={"full"}
+              h={200}
+              src={item.imageUri}
+              alt="Green double couch with wooden legs"
+              borderRadius="lg"
+            />
+            <Stack mt="3" spacing="2">
+              <Heading size="md" color="text.900" fontWeight="semibold">
+                {item.courseName}
+              </Heading>
+              <Text textColor="text.900" fontSize="sm" fontWeight="normal">
+                {item.authorName}
+              </Text>
+              <Text color="text.900" fontSize="sm" fontWeight="thin">
+                {item.rating}
+              </Text>
+              <Text color="text.900" fontSize="sm" fontWeight="semibold">
+                ${item.price}
+              </Text>
+            </Stack>
+          </CardBody>
+          <Divider />
+          {!item.isBought ? (
+            <CardFooter>
+              <Button
+                color="white"
+                backgroundColor="primary.900"
+                _hover={{ backgroundColor: "hover.900" }}
+                onClick={() => {
+                  router.push({
+                    pathname: item.isInstructor
+                      ? "/instructor/course-edit"
+                      : "/student/course-info",
+                    query: { _id: item._id },
+                  });
+                }}
+              >
+                See Details
+              </Button>
+            </CardFooter>
+          ) : null}
+        </ChakraBox>
+      </Card>
+    </Container>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,86 +1,81 @@
-import React, { useEffect, useState } from "react";
-import { Link, Text, VStack, HStack } from "@chakra-ui/react";
+import React from "react";
+import { Link, Text, VStack } from "@chakra-ui/react";
 import Courses from "../components/Courses";
 import Reviews from "../components/Reviews";
 import { getCourseListService } from "../services/courseService";
-import { loaderStore } from "../store/loaderStore";
 import FAQ from "../components/FAQ";
-// import { motion, useScroll } from "framer-motion";
+
 export default function Home(props) {
-  //console.log("server side props in index", props);
   const { courseList } = props;
   return (
-    <>
-      <VStack
-        alignItems={"center"}
-        w="full"
-        minH="70vh"
-        backgroundColor="background.900"
-        spacing={5}
-        pt={5}
-        pb={6}
-        px={10}
-      >
-        <VStack alignItems={"center"} w="full">
-          <Text
-            textAlign="center"
-            fontWeight="extrabold"
-            fontSize="2xl"
-            color="text.900"
-            textDecoration="underline"
-            textDecorationColor="primary.900"
-          >
-            Learn{" "}
-            <Link color="primary.900" textDecoration="none">
-              in-demand{" "}
-            </Link>{" "}
-            professional skills
-          </Text>
-          <Text
-            textAlign="center"
-            fontWeight="medium"
-            fontSize="xl"
-            color="text.900"
-          >
-            Choose from courses in English and many other languages
-          </Text>
-        </VStack>
-
-        <VStack w="full" mt={3} spacing={5}>
-          <Courses list={courseList} />
-          <Text
-            fontSize="2xl"
-            color="text.900"
-            fontWeight="medium"
-            textDecoration="underline"
-            textDecorationColor="primary.900"
-          >
-            What Students have to say
-          </Text>
-          <Reviews isStudent={true} />
-          <Text
-            fontSize="2xl"
-            color="text.900"
-            fontWeight="medium"
-            textDecoration="underline"
-            textDecorationColor="primary.900"
-          >
-            What Students are Looking into
-          </Text>
-          <Courses list={courseList} />
-          <Text
-            fontSize="2xl"
-            color="text.900"
-            fontWeight="medium"
-            textDecoration="underline"
-            textDecorationColor="primary.900"
-          >
-            Frequently Asked Questions
-          </Text>
-          <FAQ />
-        </VStack>
+    <VStack
+      alignItems={"center"}
+      w="full"
+      minH="70vh"
+      backgroundColor="background.900"
+      spacing={5}
+      pt={5}
+      pb={6}
+      px={10}
+    >
+      <VStack alignItems={"center"} w="full">
+        <Text
+          textAlign="center"
+          fontWeight="extrabold"
+          fontSize="2xl"
+          color="text.900"
+          textDecoration="underline"
+          textDecorationColor="primary.900"
+        >
+          Learn{" "}
+          <Link color="primary.900" textDecoration="none">
+            in-demand{" "}
+          </Link>{" "}
+          professional skills
+        </Text>
+        <Text
+          textAlign="center"
+          fontWeight="medium"
+          fontSize="xl"
+          color="text.900"
+        >
+          Choose from courses in English and many other languages
+        </Text>
       </VStack>
-    </>
+      <VStack w="full" mt={3} spacing={5}>
+        <Courses list={courseList} />
+        <Text
+          fontSize="2xl"
+          color="text.900"
+          fontWeight="medium"
+          textDecoration="underline"
+          textDecorationColor="primary.900"
+        >
+          What Students have to say
+        </Text>
+        <Reviews isStudent={true} />
+        <Text
+          fontSize="2xl"
+          color="text.900"
+          fontWeight="medium"
+          textDecoration="underline"
+          textDecorationColor="primary.900"
+        >
+          What Students are Looking into
+        </Text>
+        <Courses list={courseList} />
+        <Text
+          fontSize="2xl"
+          color="text.900"
+          fontWeight="medium"
+          textDecoration="underline"
+          textDecorationColor="primary.900"
+        >
+          Frequently Asked Questions
+        </Text>
+        <FAQ />
+      </VStack>
+    </VStack>
   );
 }
 export async function getStaticProps() {
@@ -89,7 +84,6 @@ export async function getStaticProps() {
     return { redirect: { destination: "/auth-user/login", permanent: false } };
   }
   const courseList = response.courses;
-
   return {
     props: {
       courseList,
