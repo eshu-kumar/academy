3d55b50d7e8e97365083ee0f65565faaf04309b4
sankalp nihal pandey <sankalp@geekyants.com>
2023-02-06 18:47:49 +0530

    seo experiment

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,74 @@
 // pages/_app.js
 import { ChakraProvider } from "@chakra-ui/react";
 // 1. Import the extendTheme function
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 import Layout from "../components/layouts/Layout";
+import Head from "next/head";
 // 2. Extend the theme to include custom colors, fonts, etc
+//use below color code for black theme
+// const colors = {
+//   primary: {
+//     900: "#e32551",
+//     600: "#de3163",
+//   },
+//   background: {
+//     900: "#000000",
+//     700: "#1A1C21",
+//   },
+//   text: {
+//     900: "#FFFFFF",
+//   },
+//   hover: {
+//     900: "#f60057",
+//   },
+// };
+
+//use below color code for white theme
 const colors = {
+  background: {
+    900: "#b9ac92",
+  },
   primary: {
     900: "#e32551",
-    800: "#153e75",
-    700: "#2a69ac",
     600: "#de3163",
   },
-  background: {
-    900: "#000000",
-    700: "#1A1C21",
-  },
   text: {
-    900: "#FFFFFF",
+    900: "#ffffff",
   },
   hover: {
     900: "#f60057",
   },
-  border: {},
+};
+
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "light",
 };
 
 const theme = extendTheme({ colors });
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Welcome To Academy</title>
+        <meta property="og:title" content="Welcome To Academy" key="title" />
+        <meta
+          name="description"
+          content="Online Learning Platform online lectures"
+        />
+        <meta
+          name="keywords"
+          content="HTML, CSS, JavaScript,coding,onlineclasses, lecture"
+        />
+        <meta name="author" content="Academy" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
   );
 }
 
