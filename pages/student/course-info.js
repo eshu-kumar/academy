47d98f6da186ccdb2d044e838aad29086a6b1462
sankalp nihal pandey <sankalp@geekyants.com>
2023-02-07 12:57:33 +0530

    Seo experiment

--- pages/student/course-info.js
+++ pages/student/course-info.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import {
-  Text,
   Button,
   HStack,
   VStack,
@@ -17,9 +16,20 @@ import { getCourseInfoService } from "../../services/courseService";
 import Lectures from "../../components/Lectures";
 import Reviews from "../../components/Reviews";
 import { loaderStore } from "../../store/loaderStore";
+import { NextSeo } from "next-seo";
 import { authStore } from "../../store/authStore";
 import CourseOverview from "../../components/CourseOverview";
 import MyTooltip from "../../components/others/MyTooltip";
+const SEO = {
+  title: "MY learning page",
+  description:
+    "Welcome to your learning page here you will see all your active courses that you bought",
+  openGraph: {
+    title: "MY learning page",
+    description:
+      "Welcome to your learning page here you will see all your active courses that you bought",
+  },
+};
 export default function CourseInfo(props) {
   const auth = authStore();
   const router = useRouter();
@@ -46,94 +56,96 @@ export default function CourseInfo(props) {
     : "https://images.unsplash.com/photo-1484950763426-56b5bf172dbb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGhkJTIwcGhvdG9zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60";
   if (course)
     return (
-      <Flex minH="90vh" width="full" backgroundColor="background.900">
-        <VStack w="full" alignItems="flex-start">
-          <Image
-            w={"full"}
-            h={400}
-            src={uri}
-            alt="Green double couch with wooden legs"
-            borderRadius="lg"
-          />
-          <VStack w="full" alignItems="flex-start" spacing={1} px={6} pt={2}>
-            <HStack spacing={2} w="full" justifyContent="flex-end">
-              <MyTooltip message="Congratulations in Trial version you can Watch this course for Free now!">
-                <Button
-                  // variant='ghost'
-                  color="white"
-                  backgroundColor="primary.900"
-                  _hover={{ backgroundColor: "hover.900" }}
-                  onClick={() => {
-                    if (course && course._id) {
-                      router.push({
-                        pathname: "../view-course",
-                        query: { _id: course._id },
-                      });
-                    }
-                  }}
-                >
-                  Watch Course
-                </Button>
-              </MyTooltip>
+      <>
+        <NextSeo {...SEO} />
+        <Flex minH="90vh" width="full" backgroundColor="background.900">
+          <VStack w="full" alignItems="flex-start">
+            <Image
+              w={"full"}
+              h={400}
+              src={uri}
+              alt="Green double couch with wooden legs"
+              borderRadius="lg"
+            />
+            <VStack w="full" alignItems="flex-start" spacing={1} px={6} pt={2}>
+              <HStack spacing={2} w="full" justifyContent="flex-end">
+                <MyTooltip message="Congratulations in Trial version you can Watch this course for Free now!">
+                  <Button
+                    // variant='ghost'
+                    color="white"
+                    backgroundColor="primary.900"
+                    _hover={{ backgroundColor: "hover.900" }}
+                    onClick={() => {
+                      if (course && course._id) {
+                        router.push({
+                          pathname: "../view-course",
+                          query: { _id: course._id },
+                        });
+                      }
+                    }}
+                  >
+                    Watch Course
+                  </Button>
+                </MyTooltip>
 
-              <MyTooltip message="Congratulations in Trial version you can Enroll this course for Free now!">
-                <Button
-                  // variant='ghost'
-                  color="white"
-                  backgroundColor="primary.900"
-                  _hover={{ backgroundColor: "hover.900" }}
-                  onClick={() => {
-                    if (course && course._id) {
-                      router.push({
-                        pathname: "../student/my-learnings",
-                        query: { _id: course._id },
-                      });
-                    }
-                  }}
-                >
-                  Enroll
-                </Button>
-              </MyTooltip>
-            </HStack>
-            <Tabs width="full" px={2}>
-              <TabList overflowX={"auto"}>
-                <Tab
-                  borderTopRadius="md"
-                  _selected={{ bg: "primary.900" }}
-                  color="text.900"
-                >
-                  Overview
-                </Tab>
-                <Tab
-                  borderTopRadius="md"
-                  _selected={{ bg: "primary.900" }}
-                  color="text.900"
-                >
-                  Content
-                </Tab>
-                <Tab
-                  borderTopRadius="md"
-                  _selected={{ bg: "primary.900" }}
-                  color="text.900"
-                >
-                  Reviews
-                </Tab>
-              </TabList>
-              <TabPanels>
-                <TabPanel>
-                  {course ? <CourseOverview course={course} /> : null}
-                </TabPanel>
-                <TabPanel>
-                  <Lectures userEmail={auth.email} lectures={lectures} />
-                </TabPanel>
-                <TabPanel>
-                  <Reviews />
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
+                <MyTooltip message="Congratulations in Trial version you can Enroll this course for Free now!">
+                  <Button
+                    color="white"
+                    backgroundColor="primary.900"
+                    _hover={{ backgroundColor: "hover.900" }}
+                    onClick={() => {
+                      if (course && course._id) {
+                        router.push({
+                          pathname: "../student/my-learnings",
+                          query: { _id: course._id },
+                        });
+                      }
+                    }}
+                  >
+                    Enroll
+                  </Button>
+                </MyTooltip>
+              </HStack>
+              <Tabs width="full" px={2}>
+                <TabList overflowX={"auto"}>
+                  <Tab
+                    borderTopRadius="md"
+                    _selected={{ bg: "primary.900" }}
+                    color="text.900"
+                  >
+                    Overview
+                  </Tab>
+                  <Tab
+                    borderTopRadius="md"
+                    _selected={{ bg: "primary.900" }}
+                    color="text.900"
+                  >
+                    Content
+                  </Tab>
+                  <Tab
+                    borderTopRadius="md"
+                    _selected={{ bg: "primary.900" }}
+                    color="text.900"
+                  >
+                    Reviews
+                  </Tab>
+                </TabList>
+                <TabPanels>
+                  <TabPanel>
+                    {course ? <CourseOverview course={course} /> : null}
+                  </TabPanel>
+                  <TabPanel>
+                    <Lectures userEmail={auth.email} lectures={lectures} />
+                  </TabPanel>
+                  <TabPanel>
+                    <Reviews />
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
+            </VStack>
           </VStack>
-        </VStack>
-      </Flex>
+        </Flex>
+      </>
     );
 }
 export async function getServerSideProps(context) {
--- pages/student/my-learnings.js
+++ pages/student/my-learnings.js
@@ -1,83 +1,94 @@
-import React, { useState, useEffect } from "react";
-import {
-  Flex,
-  Center,
-  Text,
-  Button,
-  Box,
-  VStack,
-  HStack,
-} from "@chakra-ui/react";
+import React from "react";
+import { Text, Box, VStack } from "@chakra-ui/react";
 import { getCourseListService } from "../../services/courseService";
 import Courses from "../../components/Courses";
-import { loaderStore } from "../../store/loaderStore";
+import { NextSeo } from "next-seo";
 import { authStore } from "../../store/authStore";
 import { authenticateServerService } from "../../services/authService";
+const SEO = {
+  title: "MY learning page",
+  description:
+    "Welcome to your learning page here you will see all your active courses that you bought",
+  openGraph: {
+    title: "MY learning page",
+    description:
+      "Welcome to your learning page here you will see all your active courses that you bought",
+  },
+};
 export default function MyLearnings(props) {
   const auth = authStore();
   //console.log("props in my learning", props);
   const { courseList } = props;
   return (
-    <VStack
-      p={[4, 6, 6]}
-      minH={"70vh"}
-      spacing={2}
-      w="full"
-      bg="background.900"
-      px={4}
-    >
-      <Text
-        color="text.900"
-        fontWeight="bold"
-        fontSize="2xl"
-        textAlign="center"
-        pt={6}
+    <>
+      <NextSeo {...SEO} />
+      <VStack
+        p={[4, 6, 6]}
+        minH={"70vh"}
+        spacing={2}
+        w="full"
+        bg="background.900"
+        px={4}
       >
-        Welcome back {auth.email ? auth.email.split("@")[0].split(".")[0] : ""}
-      </Text>
-      <Box borderWidth={1} borderColor="primary.900" p={4} alignItems="center">
         <Text
+          color="text.900"
+          fontWeight="bold"
+          fontSize="2xl"
           textAlign="center"
+          pt={6}
+        >
+          Welcome back{" "}
+          {auth.email ? auth.email.split("@")[0].split(".")[0] : ""}
+        </Text>
+        <Box
+          borderWidth={1}
+          borderColor="primary.900"
+          p={4}
+          alignItems="center"
+        >
+          <Text
+            textAlign="center"
+            color="text.900"
+            fontWeight="semibold"
+            fontSize="lg"
+          >
+            Learning that gets you skills for your present and for future
+          </Text>
+        </Box>
+        <Text
+          color="text.900"
+          fontWeight="bold"
+          fontSize="2xl"
+          textAlign="start"
+          textDecoration="underline"
+          textDecorationColor="primary.900"
+        >
+          My Active Courses
+        </Text>
+        <Courses list={courseList} isBought={true} />
+        <Text
+          color="text.900"
+          fontWeight="bold"
+          fontSize="3xl"
+          textAlign="start"
+          textDecoration="underline"
+          textDecorationColor="primary.900"
+        >
+          What To Learn Next
+        </Text>
+        <Text
           color="text.900"
           fontWeight="semibold"
-          fontSize="lg"
+          fontSize="2xl"
+          textAlign="start"
+          textDecoration="underline"
+          textDecorationColor="primary.900"
         >
-          Learning that gets you skills for your present and for future
+          Students Are Also Viewing
         </Text>
-      </Box>
-      <Text
-        color="text.900"
-        fontWeight="bold"
-        fontSize="2xl"
-        textAlign="start"
-        textDecoration="underline"
-        textDecorationColor="primary.900"
-      >
-        My Active Courses
-      </Text>
-      <Courses list={courseList} isBought={true} />
-      <Text
-        color="text.900"
-        fontWeight="bold"
-        fontSize="3xl"
-        textAlign="start"
-        textDecoration="underline"
-        textDecorationColor="primary.900"
-      >
-        What To Learn Next
-      </Text>
-      <Text
-        color="text.900"
-        fontWeight="semibold"
-        fontSize="2xl"
-        textAlign="start"
-        textDecoration="underline"
-        textDecorationColor="primary.900"
-      >
-        Students Are Also Viewing
-      </Text>
-      <Courses list={courseList} />
-    </VStack>
+        <Courses list={courseList} />
+      </VStack>
+    </>
   );
 }
 export async function getServerSideProps(context) {
